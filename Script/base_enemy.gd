43076eab900a983cf37be53821191051cbca5e09
Octodice <thomas2dumont@gmail.com>
2026-07-22 22:39:06 +0200

    adding avoindance to enemies nav agent

--- Script/base_enemy.gd
+++ Script/base_enemy.gd
@@ -7,16 +7,32 @@ class_name Enemy
 
 var player: Player
 var last_direction: Vector2 = Vector2.ZERO
-
+var movement_delta: float
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_tree().get_first_node_in_group("Player")
+	nav_agent.velocity_computed.connect(_on_velocity_computed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+	# Do not query when the map has never synchronized and is empty.
+	if NavigationServer2D.map_get_iteration_id(nav_agent.get_navigation_map()) == 0:
+		return
+
 	nav_agent.target_position = player.global_position
-	var next_position = global_position.move_toward(nav_agent.get_next_path_position(), delta * SPEED)
-	last_direction = global_position.direction_to(next_position).normalized()
-	global_position = next_position
+	movement_delta = delta * SPEED
+
+	var next_path_position: Vector2 = nav_agent.get_next_path_position()
+	var new_velocity: Vector2 = global_position.direction_to(next_path_position) * movement_delta
+	if nav_agent.avoidance_enabled:
+		nav_agent.set_velocity(new_velocity)
+	else:
+		_on_velocity_computed(new_velocity)
+
+	last_direction = new_velocity.normalized()
+
 
 func get_last_direction(): return last_direction
+
+func _on_velocity_computed(safe_velocity: Vector2):
+	global_position = global_position.move_toward(global_position + safe_velocity, movement_delta)
