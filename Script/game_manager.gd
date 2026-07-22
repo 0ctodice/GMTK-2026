2b02740aff70335ae48c060b41cc4da82ca6dcec
Octodice <thomas2dumont@gmail.com>
2026-07-22 23:18:10 +0200

    added a fixed random number of spawns per levels

--- Script/game_manager.gd
+++ Script/game_manager.gd
@@ -6,16 +6,22 @@ extends Node2D
 
 var rng: RandomNumberGenerator
 var screen_size: Vector2
+
+var max_spawners_number: int
+var total_spawners: int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rng = RandomNumberGenerator.new()
 	screen_size = get_viewport().get_visible_rect().size
 	spawn_timer.timeout.connect(_create_spawners)
+	max_spawners_number = rng.randi_range(5, 12)
 
 func _create_spawners():
-	var spawn_number: int = rng.randi_range(1, 3)
+	spawn_timer.stop()
+	var spawner_number: int = rng.randi_range(1, 3)
 
-	for i in range(spawn_number):
+	for i in range(spawner_number):
 		var rand_x = rng.randf_range(enemy_sprite_size.x, screen_size.x - enemy_sprite_size.x)
 		var rand_y = rng.randf_range(enemy_sprite_size.y, screen_size.y - enemy_sprite_size.y)
 		
@@ -23,4 +29,6 @@ func _create_spawners():
 		spawn_instance.global_position = Vector2(rand_x, rand_y)
 		add_child(spawn_instance)
 
-	spawn_timer.start(rng.randf_range(1, 5))
+	total_spawners += spawner_number
+	if total_spawners < max_spawners_number:
+		spawn_timer.start(rng.randf_range(1, 5))
